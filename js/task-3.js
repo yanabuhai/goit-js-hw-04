af76c698ba57e02b9a7ef8141326d38392e880ec
Yana Buhai <buhaiyanaol@gmail.com>
2024-06-13 17:01:30 +0300

    add-all

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,15 @@
 const profile = {
   username: "Jacob",
   playTime: 300,
-
-    changeUsername(newName) {
-        for (const user of this.username) {
-            if (user.name === username) {
-                user.name = newName;
-            }
-        },
-    }   
-    updatePlayTime(hours){
-            this.hours.push.playTime;
-        }
-     getInfo() {
-         return `${Username} has ${amount} active hours!`;
-     }
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
